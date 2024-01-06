e5cb56a7779bb090727fd1d73f41ea600542ef3e
Maria Koelbel <mariackoelbel@gmail.com>
2024-01-06 13:32:35 -0500

    rename recipes_data_list to recipes_list

--- functions/get_cocktail_names.R
+++ functions/get_cocktail_names.R
@@ -1,5 +1,5 @@
-get_cocktail_names <- function(recipes_data_list) {
-  recipe_name_list <- sapply(recipes_data_list, function(recipe) {
+get_cocktail_names <- function(recipes_list) {
+  recipe_name_list <- sapply(recipes_list, function(recipe) {
     recipe[["name"]]
   })
   recipe_name_list
--- functions/get_recipes_list.R
+++ functions/get_recipes_list.R
@@ -5,16 +5,16 @@ get_recipes_list <- function(recipes_text_file = RECIPES_TEXT_FILE,
   
   recipes_text <- read_file(recipes_text_file)
   recipes_text_list <- remove_blanks_from_vector(unlist(strsplit(recipes_text, recipe_starter)))
-  recipes_data_list <- vector("list", length = length(recipes_text_list))
+  recipes_list <- vector("list", length = length(recipes_text_list))
   
   lapply(seq(recipes_text_list), function(r) {
     recipe <<- recipes_text_list[r]
-    recipes_data_list[[r]]$name <<- extract_recipe_name(recipe)
-    recipes_data_list[[r]]$ingredients <<- extract_recipe_ingredients(recipe)
-    recipes_data_list[[r]]$instructions <<- extract_recipe_instructions(recipe)
-    recipes_data_list[[r]]$tags <<- extract_recipe_tags(recipe) #%>% replace("", NA)
-    recipes_data_list[[r]]$scalable <<- extract_recipe_scalability(recipe)
+    recipes_list[[r]]$name <<- extract_recipe_name(recipe)
+    recipes_list[[r]]$ingredients <<- extract_recipe_ingredients(recipe)
+    recipes_list[[r]]$instructions <<- extract_recipe_instructions(recipe)
+    recipes_list[[r]]$tags <<- extract_recipe_tags(recipe) #%>% replace("", NA)
+    recipes_list[[r]]$scalable <<- extract_recipe_scalability(recipe)
   })
   
-  recipes_data_list
+  recipes_list
 }
